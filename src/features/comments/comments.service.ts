8ef2eeffc1d695679c25c0af01ad40806034f4bb
Maurício Azevedo <mauricio.azevedo@citechnology.com.br>
2023-08-25 13:52:59 -0300

    feat: comment | inject model onto service

--- src/features/comments/comments.service.ts
+++ src/features/comments/comments.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PostCommentDto } from './dto/post-comment.dto';
 import { PutCommentDto } from './dto/put-comment.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Comment } from './schemas/comment.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class CommentsService {
+  constructor(@InjectModel(Comment.name) private userModel: Model<Comment>) {}
   create(postCommentDto: PostCommentDto) {
     return 'This action adds a new comment';
   }
